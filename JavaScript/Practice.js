d1eabf4652c9fbeefea45c4210782ad8bd86cb83
unknown <zyi.commerce@outlook.com>
2022-02-12 07:09:16 -0700

    update on JavaScript

--- JavaScript/Practice.js
+++ JavaScript/Practice.js
@@ -1,5 +1,30 @@
-console.log("HELLO FROM OUR FIRST JS FILE!!!")
+// console.log("HELLO FROM OUR FIRST JS FILE!!!")
 
-if (1 + 1 === 2) {
-    console.log("Math is very useful!")
+// let random = Math.random;
+
+// if (random < 0.5) {
+//     console.log("Math is very useful!")
+// }
+
+// const password = prompt("Please enter a new password");
+
+// // 6 chars
+// if (password.length <= 6) {
+//     console.log("Password too short")
+// }
+
+// if (password.indexOf(' ') === -1) {
+//     console.log
+// }
+
+const day = 8;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    default:
+        console.log("not a day!");
 }
