68e92c58423e408633da4477fad3ebdf2ed5563b
unknown <zyi.commerce@outlook.com>
2022-02-26 17:23:28 -0700

    JS functions

---
+++ JavaScript/Function/app_function.js
@@ -0,0 +1,49 @@
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}
+
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+}
+
+function repeat(str, numTimes) {
+    let result = '';
+    for (let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result);
+}
+
+function add(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return false;
+    }
+    return x + y;
+}
+
+
+function rant(message) {
+    console.log(message.toUpperCase());
+}
+
+function capitalize(string) {
+    let first = string.slice(0, 1).toUpperCase();
+    let rest = string.slice(1, string.length);
+    return first.concat(rest);
+}
+
+
+singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+
