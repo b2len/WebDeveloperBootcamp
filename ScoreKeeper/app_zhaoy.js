1dd1de5e0b6ef89c1b46b6e258395a0ca06c8707
unknown <zyi.commerce@outlook.com>
2022-03-25 12:06:42 -0600

    Score Keeper

--- ScoreKeeper/app_zhaoy.js
+++ ScoreKeeper/app_zhaoy.js
@@ -1,14 +1,15 @@
 const p1Button = document.querySelector('#p1Button')
 const p2Button = document.querySelector('#p2Button')
-const resetButton = document.querySelector('#Reset')
+const resetButton = document.querySelector('#reset')
 const p1Display = document.querySelector('#p1Display')
 const p2Display = document.querySelector('#p2Display')
 const winningScoreSelect = document.querySelector('#playto')
 
 let p1Score = 0;
 let p2Score = 0;
-let WinningScore = 5;
+let WinningScore = 3;
 let isGameOver = false;
+
 
 p1Button.addEventListener('click', function () {
     if (!isGameOver) {
@@ -17,6 +18,8 @@ p1Button.addEventListener('click', function () {
             isGameOver = true;
             p1Display.classList.add('winner');
             p2Display.classList.add('loser');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p1Display.textContent = p1Score;
     }
@@ -30,6 +33,8 @@ p2Button.addEventListener('click', function () {
             isGameOver = true;
             p2Display.classList.add('winner');
             p1Display.classList.add('loser');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Score;
     }
@@ -49,4 +54,6 @@ function reset() {
     p2Score = 0;
     p1Display.classList.remove('winner', 'loser');
     p2Display.classList.remove('winner', 'loser');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 }
