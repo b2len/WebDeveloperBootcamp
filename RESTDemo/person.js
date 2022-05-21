90c957e9fcd23b428eb54970dd7ba634274ba904
unknown <zyi.commerce@outlook.com>
2022-05-21 15:54:34 -0600

    Mongoose Basics

---
+++ RESTDemo/person.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!!!!")
+        console.log(err)
+    })
+
+const personSchema = new mongoose.Schema({
+    first: String,
+    last: String
+})
